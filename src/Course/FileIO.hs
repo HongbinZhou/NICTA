acbd9148f2b85094c618ff550da55f47f27c2e1a
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-04-11 11:34:38 +0800

    can print when put files in the root dir. neet improve.

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -62,10 +62,16 @@ the contents of c
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
--- main = getArgs >>= test
-main = error "todo"
+main = getArgs >>= test1
+-- main = -- getArgs >>= f
+  -- error "todo"
 
-test a = (void $ sequence $ run <$> a)
+-- test by:
+--         test1 ("./share/files.txt":.Nil :: List Filename)
+test1 a = void $ sequence$  test <$> a
+test a = readFile a >>= run
+
+-- test a = (void $ sequence $ run <$> a)
 
 type FilePath =
   Chars
@@ -75,17 +81,23 @@ run ::
   Chars
   -> IO ()
 run a = (getFiles $ lines a) >>= printFiles
+-- test by: 
+--         run "share/a.txt\nshare/b.txt"
 
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles a = 
   sequence $ getFile <$> a
+-- test by: 
+--         getFiles ("share/a.txt":."share/b.txt":.Nil ::List FilePath)
 
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
 getFile a = ((,) a) <$> readFile a
+-- test by:
+--        getFile "share/a.txt"
 
 printFiles ::
   List (FilePath, Chars)
