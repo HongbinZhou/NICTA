f11403cd7c5827bf8739d6a020b0a356d9a2d11f
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-05-05 21:34:04 +0800

    done apply and bind for Compose. No fmap.

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -17,23 +17,25 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo"
+  (<$>) = error "todo"
 
 instance (Apply f, Apply g) =>
   Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo"
+  -- Note:
+  --      Compose f (g (a->b)) <*> Compose f (g a) = Compose f (g b)
+  --      <*> :: f (a -> b) -> f a -> f b
+  --      lift2 :: Apply f => (a -> b -> c) -> f a -> f b -> f c
+  (Compose f) <*> (Compose a) = Compose (lift2 (<*>) f a)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo"
+  pure x = Compose (pure (pure x))
 
 instance (Bind f, Bind g) =>
   Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
-  (=<<) =
-    error "todo"
+  f =<< x = do
+    a <- x
+    f a
